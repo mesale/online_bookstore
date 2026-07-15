3d8293a7d328f5805d167ca487d8ff61f4684213
mesale <mesalemtsegaye@gmail.com>
2026-07-15 03:29:54 +0300

    feat: update environment configurations and remove deprecated files for deployment

--- OnlineBookStoreApp/OnlineBookstoreFrontend/online-bookstore/src/auth/keycloak.js
+++ OnlineBookStoreApp/OnlineBookstoreFrontend/online-bookstore/src/auth/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: import.meta.env.VITE_KEYCLOAK_URL || "http://localhost:8080",
+  url: import.meta.env.VITE_KEYCLOAK_URL || "https://aexin-196-188-242-147.free.pinggy.net",
   realm: import.meta.env.VITE_KEYCLOAK_REALM || "bookstore-realm",
   clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID || "bookstore-frontend",
 });
